fe786442794eee1873483e1f1ce38b253a19d4b8
fenota-viktoriya <Fen9206@gmail.com>
2023-05-06 11:56:12 +0300

    added modal

--- src/menu.js
+++ src/menu.js
@@ -22,6 +22,6 @@
     if (!e.matches) return;
     mobileMenu.classList.remove("is-open");
     openMenuBtn.setAttribute("aria-expanded", false);
-    bodyScrollLock.enableBodyScroll(document.body);
+    // bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
